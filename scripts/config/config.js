7180403897cbe4e57f63712f8e63650173b67ed5
Ayu Adiati <retno.ayu98@gmail.com>
2026-03-27 11:16:31 +0100

    Initial template setup

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -1,7 +1,7 @@
 // --- Core Configuration Variables ---
 // Change GITHUB_USERNAME and SINCE_YEAR to set the starting year for your contribution history.
-const GITHUB_USERNAME = 'adiati98';
-const SINCE_YEAR = 2019;
+const GITHUB_USERNAME = 'your-github-username';
+const SINCE_YEAR = 2024;
 const BASE_URL = 'https://api.github.com';
 
 // --- Configuration to generate README in the contributions folder ---
