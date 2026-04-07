176d8f9732a3056fa43275b6b192a3dd3e8a4578
Ayu Adiati <retno.ayu98@gmail.com>
2026-04-07 11:41:06 +0200

    add username

--- scripts/config/config.js
+++ scripts/config/config.js
@@ -1,6 +1,6 @@
 // --- Core Configuration Variables ---
 // GITHUB_USERNAME is used to fetch and identify your contribution history.
-const GITHUB_USERNAME = 'your-github-username';
+const GITHUB_USERNAME = 'adiati98';
 const BASE_URL = 'https://api.github.com';
 
 // --- Configuration to generate README in the contributions folder ---
